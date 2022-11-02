69edc8919a26b1c5364b25fa302636a331e3d9f4
alimaher12 <ali840085@gmail.com>
2022-11-02 08:57:27 +0200

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -146,7 +146,7 @@
 <body>
 
     <article>
-        <h1>Ali Maher</h1>
+        <h1><a href="/single_post"> Ali Maher</a></h1>
         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
             industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
             scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
