ecb9e9f6d58b60e0c0353403d1690aced91b4281
alimaher12 <ali840085@gmail.com>
2022-11-07 21:27:39 +0200

    update

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
 
@@ -15,25 +16,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('posts');
+
+    return view('posts', [
+        'posts' =>  Post::all(),
+    ]);
 });
 
 
-Route::get('posts/{post}', function ($slug) {
-
-    if (!file_exists($path = __DIR__ . '/../resources/posts/' . $slug . '.html')) {
-
-        abort(404);
-    }
-
-    $post = Cache::remember(
-        'posts.{$slug}',
-        now()->addMinutes(),
-        fn () => file_get_contents($path)
-    );
-
+Route::get('posts/{post}', function ($id) {
     return view('post', [
-        'post' => $post,
+        'post' => Post::findOrFail($id)
     ]);
-})->where('post', '[A-z_\-]+');
-
+});
